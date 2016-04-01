08090dc888c4ddd34fc9875ae8b5fa50fcf9b4ed
Christof Koenig <koenig@crealytics.de>
2016-04-01 11:54:21 +0200

    Return nil instead of failing.

--- lib/facter/augeasprovider_grub_version.rb
+++ lib/facter/augeasprovider_grub_version.rb
@@ -1,11 +1,12 @@
 Facter.add(:augeasprovider_grub_version) do
-  if File.exists?('/etc/default/grub')
-    version = 2
-  elsif File.exists?('/boot/efi/EFI/redhat/grub.conf') || File.exists?('/boot/grub/menu.lst')
-    version = 1
-  else
-    fail('Failed to evaluate grub version')
-  end
+  version =
+    if File.exists?('/etc/default/grub')
+      2
+    elsif File.exists?('/boot/efi/EFI/redhat/grub.conf') || File.exists?('/boot/grub/menu.lst')
+      1
+    else
+      nil
+    end
 
   setcode { version }
 end
