5e2d464cee5ee723c4835b7524e4faca591cee8e
Trevor Vaughan <trevor-vaughan@users.noreply.github.com>
2017-08-29 14:54:36 -0700

    Add Global EFI support (#28)
    * Updated EFI support
    * Added tests for Puppet 5 and Ruby 2.4.0
    * Added compatiblity with CentOS
    * Restricted RHEL and CentOS versions to those that can be tested
    * Eliminated old Ruby versions from the tests since they fundamentally
      break
    
    Closes #27

--- lib/puppet/provider/kernel_parameter/grub2.rb
+++ lib/puppet/provider/kernel_parameter/grub2.rb
@@ -154,9 +154,31 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2, :parent => Puppet::Type.typ
   end
 
   def flush
+    os_info = Facter.value(:os)
+    if os_info
+      os_name = Facter.value(:os)['name']
+    else
+      # Support for old versions of Facter
+      unless os_name
+        os_name = Facter.value(:operatingsystem)
+      end
+    end
+
     cfg = nil
-    ["/boot/grub/grub.cfg", "/boot/grub2/grub.cfg", "/boot/efi/EFI/fedora/grub.cfg", "/etc/grub2-efi.cfg"].each {|c|
-      cfg = c if FileTest.file? c
+    [
+      "/etc/grub2-efi.cfg",
+      # Handle the standard EFI naming convention
+      "/boot/efi/EFI/#{os_name.downcase}/grub.cfg",
+      "/boot/grub2/grub.cfg",
+      "/boot/grub/grub.cfg"
+    ].each {|c|
+      if FileTest.file?(c) || ( FileTest.symlink?(c) &&
+          FileTest.directory?(File.dirname(File.absolute_path(File.readlink(c)))) )
+
+        cfg = c
+        break
+
+      end
     }
     fail("Cannot find grub.cfg location to use with grub-mkconfig") unless cfg
 
