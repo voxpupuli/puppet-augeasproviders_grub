0ef64af1a9fa72130b14d6fe55cad58eb4ef1e61
Kenyon Ralph <kenyon@kenyonralph.com>
2020-11-26 05:09:16 -0800

    correct RedHat capitalization

--- lib/puppet/provider/kernel_parameter/grub2.rb
+++ lib/puppet/provider/kernel_parameter/grub2.rb
@@ -19,7 +19,7 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2, :parent => Puppet::Type.typ
     which("grub2-mkconfig") or which("grub-mkconfig") or '/usr/sbin/grub-mkconfig'
   end
 
-  defaultfor :osfamily => 'Redhat', :operatingsystemmajrelease => [ '7' ]
+  defaultfor :osfamily => 'RedHat', :operatingsystemmajrelease => [ '7' ]
   defaultfor :operatingsystem => 'Debian', :operatingsystemmajrelease => [ '8' ]
   defaultfor :operatingsystem => 'Ubuntu', :operatingsystemmajrelease => [ '14.04' ]
 
