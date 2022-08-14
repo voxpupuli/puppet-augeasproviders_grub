693754a882cde4f3485a6b516c68aa1eca74abe5
Trevor Vaughan <trevor@sicura.us>
2022-08-14 18:27:14 -0400

    modulesync 6.0.0

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,28 +1,10 @@
 # frozen_string_literal: true
 
-# Default to beaker_docker
-ENV['BEAKER_HYPERVISOR'] ||= 'docker'
+# Managed by modulesync - DO NOT EDIT
+# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-require 'beaker-rspec'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-# Force FIPS off for these tests since it is not relevant.
-ENV['BEAKER_fips'] = 'no'
+configure_beaker
 
-require 'simp/beaker_helpers'
-include Simp::BeakerHelpers
-
-install_puppet unless ENV['BEAKER_provision'] == 'no'
-
-RSpec.configure do |c|
-  c.include Helpers
-
-  # ensure that environment OS is ready on each host
-  fix_errata_on hosts
-
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  c.before :suite do
-    copy_fixture_modules_to(hosts)
-  end
-end
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
