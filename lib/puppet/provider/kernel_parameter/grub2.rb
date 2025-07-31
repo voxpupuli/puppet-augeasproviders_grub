3209037afd7a5dff10d9b4a621fb71689720bf25
Mike Riddle <mike@sicura.us>
2025-07-31 01:57:53 -0400

    Add support for EL10 (#113)
    * (#112) Allowed el10 to use --update-bls-cmdline in the mkconfig command
    
    Fixes #112
    
    * Fixed a typo in the major release check and fixed a couple of rubocop issues

--- lib/puppet/provider/kernel_parameter/grub2.rb
+++ lib/puppet/provider/kernel_parameter/grub2.rb
@@ -34,7 +34,7 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2, parent: Puppet::Type.type(:
     # Fedora and Amazon Linux lack support and are excluded
     # since they don't have a release with major version 9
     needs_bls_cmdline = os.is_a?(Hash) && os['family'] == 'RedHat' &&
-                        os['release']['major'].to_i == 9 && os['release']['minor'].to_i >= 3
+                        ((os['release']['major'].to_i == 9 && os['release']['minor'].to_i >= 3) || (os['release']['major'].to_i >= 10))
 
     cmdline = [mkconfig_path]
     cmdline << '--update-bls-cmdline' if needs_bls_cmdline
